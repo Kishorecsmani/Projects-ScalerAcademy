44f935652b1637b8dfce35a0b07bc65c53be2599
kishorecsmani <kishorecsmani@gmail.com>
2022-04-03 21:54:33 +0800

    FirstFile

---
+++ AgeCheck.java
@@ -0,0 +1,17 @@
+/* Program to check whether value present in age variable is eligible 
+for voting*/
+class AgeCheck
+{
+public static void main(String[] args)
+{
+int age=32;
+if (age>=18)
+{
+System.out.println("eligible for voting");
+}
+else
+{
+System.out.println("Not eligible");
+}
+}
+}
